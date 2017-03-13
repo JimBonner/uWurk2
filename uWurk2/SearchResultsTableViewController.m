4dc28e416510a0339bedd09d87f4985310280684
JimBonner <jim@BluePlateSoftware.com>
2017-03-13 13:48:32 -0700

    SnapShot - User Defined Issues Warnings

--- uWurk2/SearchResultsTableViewController.m
+++ uWurk2/SearchResultsTableViewController.m
@@ -63,7 +63,6 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
     return [self.json count];
 }
